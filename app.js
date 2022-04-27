5a2762582378a3f507b787aa205c311b8f0abc85
NikolayAngelov96 <103751145+NikolayAngelov96@users.noreply.github.com>
2022-04-27 16:11:02 +0300

    added key handler and some styles

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const root = document.getElementById('root');
 
+const wordList = [
+    'pizza',
+    'sushi',
+    'horse',
+    'hello',
+    'water'
+]
+
+let randomIndex = Math.floor(Math.random() * wordList.length);
+const secretWord = wordList[randomIndex];
+let currentAttempt = '';
+const attempts = [];
+
+
+window.addEventListener('keydown', handleKeydown)
+
 createBoard();
 
 function createBoard() {
-    for(let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
 
@@ -15,5 +31,51 @@ function createBoard() {
         }
 
         root.appendChild(row);
+    }
+}
+
+function handleKeydown(e) {
+    const key = e.key;
+    if (e.ctrlKey || e.metaKey || e.altKey) {
+        return
+    }
+    let letter = key.toLowerCase();
+
+    if (letter === 'enter') {
+
+        if (currentAttempt.length < 5) {
+            return;
+        }
+
+        if (attempts.length < 6) {
+            attempts.push(currentAttempt);
+            currentAttempt = '';
+        }
+    } else if (letter === 'backspace') {
+        console.log('backspace pressed');
+    } else {
+        
+        console.log(letter);
+        if (currentAttempt.length < 5) {
+            currentAttempt += letter;
+        }
+    }
+    updateBoard();
+
+}
+
+function updateBoard() {
+    let row = root.firstChild;
+    for (const attempt of attempts) {
+        drawAttempt(row, attempt)
+        row = row.nextSibling;
+    }
+}
+
+function drawAttempt(row, attempt) {
+    for (let i = 0; i < 5; i++) {
+        let cell = row.children[i];
+        cell.textContent = attempt[i].toUpperCase();
+
     }
 }
